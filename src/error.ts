36d510ae4c037e3590082ac9f67a9b9394b1eb1f
jakoblorz <jakob@lorz.org>
2017-10-16 16:24:39 +0200

    removed src folder

--- src/error.ts
+++
@@ -1,8 +0,0 @@
-import { IError } from "./types";
-
-/* error definitions */
-export const FormatError: IError = { code: 400, status: "Format Error" };
-export const UnauthorizedError: IError = { code: 401, status: "Unauthorized Error" };
-export const ForbiddenError: IError = { code: 403, status: "Forbidden Error" };
-export const NotFoundError: IError = { code: 404, status: "Not Found Error" };
-export const ServerError: IError = { code: 500, status: "Server Error" };
