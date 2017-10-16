ce815b2c20391c94d70ff340b650c8e522f038e4
jakoblorz <jakob@lorz.org>
2017-10-16 17:27:12 +0200

    made some export changes

--- index.ts
+++ index.ts
@@ -2,19 +2,19 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 
 /* pure types */
 export type ErrorType = { code: number, status: string };
-export type AsyncTransactionMethod<SourceType, TargetType> = (source: SourceType) =>
+type AsyncTransactionMethod<SourceType, TargetType> = (source: SourceType) =>
     Promise<TargetType>;
-export type SyncTransactionMethod<SourceType, TargetType> = (source: SourceType) =>
+type SyncTransactionMethod<SourceType, TargetType> = (source: SourceType) =>
     TargetType;
-export type AsyncDestructionMethod<SourceType, TargetType> = (source: SourceType, req: Request, res: Response) =>
+type AsyncDestructionMethod<SourceType, TargetType> = (source: SourceType, req: Request, res: Response) =>
     Promise<TargetType>;
-export type SyncDestructionMethod<SourceType, TargetType> = (source: SourceType, req: Request, res: Response) =>
+type SyncDestructionMethod<SourceType, TargetType> = (source: SourceType, req: Request, res: Response) =>
     TargetType;
 
 /* combined types */
-export type AsyncSyncTransactionMethod<SourceType, TargetType> =
+type AsyncSyncTransactionMethod<SourceType, TargetType> =
     AsyncTransactionMethod<SourceType, TargetType> | SyncTransactionMethod<SourceType, TargetType>;
-export type AsyncSyncDestructionMethod<SourceType, TargetType> =
+type AsyncSyncDestructionMethod<SourceType, TargetType> =
     AsyncDestructionMethod<SourceType, TargetType> | SyncDestructionMethod<SourceType, TargetType>;
 
 /* error definitions */
