2380a3afa55c53c3589fd8ccf4035b0c516302b0
jakoblorz <jakob@lorz.org>
2017-10-17 12:31:27 +0200

    added isMiddlewareCallback switch to the config() curry function

--- index.ts
+++ index.ts
@@ -2,11 +2,11 @@ import { Request, RequestHandler } from "express";
 import { ErrorType } from "./src/errors";
 import { AsyncSyncDestructionMethod, AsyncSyncTransactionMethod, scaffold } from "./src/scirocco";
 
-type ScaffoldFactory = <SourceType, TargetType extends ResponseType, ResponseType>(
+type ScaffoldFactoryResult = <SourceType, TargetType extends ResponseType, ResponseType>(
     construct: AsyncSyncTransactionMethod<Request, SourceType | ErrorType>,
     callback: AsyncSyncTransactionMethod<SourceType, TargetType | ErrorType>,
     destruct?: AsyncSyncDestructionMethod<TargetType, ResponseType | ErrorType>,
-    customSuccessCode?: number) => RequestHandler;
+    isMiddlewareCallback?: boolean, customSuccessCode?: number) => RequestHandler;
 
 export { Errors, ErrorType } from "./src/errors";
 export { secure as guard } from "./src/scirocco";
@@ -15,12 +15,12 @@ export interface ISciroccoConfiguration {
     passPureErrors?: boolean;
 }
 
-export const config = (configuration: ISciroccoConfiguration): ScaffoldFactory =>
+export const config = (configuration: ISciroccoConfiguration): ScaffoldFactoryResult =>
     <SourceType, TargetType extends ResponseType, ResponseType>(
         construct: AsyncSyncTransactionMethod<Request, SourceType | ErrorType>,
         callback: AsyncSyncTransactionMethod<SourceType, TargetType | ErrorType>,
         destruct?: AsyncSyncDestructionMethod<TargetType, ResponseType | ErrorType>,
-        customSuccessCode: number = 200,
+        isMiddlewareCallback: boolean = false, customSuccessCode: number = 200,
     ): RequestHandler =>
         scaffold(construct, callback, destruct,
-            configuration.invokeNextOnError, configuration.passPureErrors, customSuccessCode);
+            configuration.invokeNextOnError, configuration.passPureErrors, customSuccessCode, isMiddlewareCallback);
