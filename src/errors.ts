d0b4e40e434f56b0e8c14d047e7dc3f251193975
jakoblorz <jakob@lorz.org>
2017-10-17 12:07:56 +0200

    added code abstraction and currying function config()

---
+++ src/errors.ts
@@ -0,0 +1,12 @@
+/* pure type */
+export type ErrorType = { code: number, status: string };
+
+// tslint:disable-next-line:no-namespace
+export namespace Errors {
+    /* error definitions */
+    export const FormatError: ErrorType = { code: 400, status: "Format Error" };
+    export const UnauthorizedError: ErrorType = { code: 401, status: "Unauthorized Error" };
+    export const ForbiddenError: ErrorType = { code: 403, status: "Forbidden Error" };
+    export const NotFoundError: ErrorType = { code: 404, status: "Not Found Error" };
+    export const ServerError: ErrorType = { code: 500, status: "Server Error" };
+}
