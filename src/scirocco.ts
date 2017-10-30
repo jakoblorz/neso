a390092250af50eeaaa94681b77763e9080218e6
jakoblorz <jakob@lorz.org>
2017-10-30 18:41:55 +0100

    added comments and clarified code

--- src/scirocco.ts
+++ src/scirocco.ts
@@ -18,27 +18,33 @@ export interface IResponseRepresentation {
     status: string;
 }
 
+/**
+ * extension of the IResponseRepresentation interface
+ * with added error capabilities
+ */
 export interface IErrorType extends IResponseRepresentation {
+
+    /**
+     * possible error
+     */
     error?: Error;
 }
 
-export interface IResponse<T> extends IErrorType {
-    error: undefined;
+/**
+ * extension of the IResponseRepresentation interface
+ * with result field
+ */
+export interface IResponse<T> extends IResponseRepresentation {
     result: T;
 }
 
 // tslint:disable-next-line:no-namespace
 export namespace Errors {
-    export const FormatError: IErrorType = {
-        code: 400, status: "Format Error", error: undefined };
-    export const UnauthorizedError: IErrorType = {
-        code: 401, status: "Unauthorized Error", error: undefined };
-    export const ForbiddenError: IErrorType = {
-        code: 403, status: "Forbidden Error", error: undefined };
-    export const NotFoundError: IErrorType = {
-        code: 404, status: "Not Found Error", error: undefined };
-    export const ServerError: IErrorType = {
-        code: 500, status: "Server Error", error: undefined };
+    export const FormatError: IErrorType = { code: 400, status: "Format Error" };
+    export const UnauthorizedError: IErrorType = { code: 401, status: "Unauthorized Error" };
+    export const ForbiddenError: IErrorType = { code: 403, status: "Forbidden Error" };
+    export const NotFoundError: IErrorType = { code: 404, status: "Not Found Error" };
+    export const ServerError: IErrorType = { code: 500, status: "Server Error" };
 }
 
 /**
@@ -51,6 +57,9 @@ export const isErrorType = (object: any): object is IErrorType =>
         "status" in object && typeof object.status === "string" &&
         "code" in object && typeof object.code === "number";
 
+/**
+ * create a new expressjs request handler using evaluation stages
+ */
 export abstract class ScaffoldedRequestHandler<RequestType extends Request, SourceType, ResultType> {
 
     /**
@@ -70,12 +79,22 @@ export abstract class ScaffoldedRequestHandler<RequestType extends Request, Sour
 
     /**
      * handler
+     * @param after specify a function which will be invoked after the callback was invoked
+     * @param invokedNextAfterExecution set flag to invoke the next function if the execution ended (e.g. as middleware)
+     * @param invokeNextOnError set flag to invoke the next function if an error occurs
+     * @param passPureErrors set flag not the exchange the errors with sendable error objects
      */
     public obtainHandler(
         after?: (result: ResultType, req: Request, res: Response, next: NextFunction) => void | Promise<void>,
         invokedNextAfterExecution: boolean = false, invokeNextOnError: boolean = false,
         passPureErrors: boolean = false): RequestHandler {
+
+            // backup this context
             const that = this;
+
+            // build a async function which invokes the extract callback, the
+            // detects format errors by invoking the guard function and then
+            // invoking the actual callback
             const transaction = async (request: RequestType): Promise<ResultType> => {
 
                 const source = await this.extract(request);
@@ -87,65 +106,51 @@ export abstract class ScaffoldedRequestHandler<RequestType extends Request, Sour
                 return this.callback(source);
             };
 
+            // return a expressjs request handler
             return async (req: Request, res: Response, next: NextFunction) => {
 
-                const result = await this.fetchPossibleErrors<ResultType>(transaction, that, req as RequestType);
-                if (isErrorType(result)) {
-
-                    if (invokeNextOnError && passPureErrors) {
-                        next(result);
-                        return;
-                    }
-
-                    if (invokeNextOnError) {
-                        next(Errors.ServerError);
-                        return;
-                    }
-
-                    if (passPureErrors) {
-                        this.respond<IErrorType>(result, res, result.code);
-                        return;
-                    }
-
-                    this.respond<IErrorType>(Errors.ServerError, res, Errors.ServerError.code);
+                // call the transaction function while catching all possible errors
+                // detect errors and handle them according to the control flow switches
+                // (invokeNextAfterExecution, invokeNextOnError, passPureErrors)
+                const result = await this.fetchPossibleErrors<ResultType>(transaction, that, req as RequestType) as
+                    ResultType;
+                if (!this.proceedExecution<ResultType>(result, invokeNextOnError, passPureErrors, res, next)) {
                     return;
                 }
 
+                // detect if a after function is present
                 if (after !== undefined) {
+
+                    // call the after function while catching all possible errors
+                    // detect errors and handle them according to the control flow switches
+                    // (invokeNextAfterExecution, invokeNextOnError, passPureErrors)
                     const error = await this.fetchPossibleErrors<void>(after, that, result, req, res, next);
-                    if (isErrorType(error)) {
-
-                        if (invokeNextOnError) {
-                            if (passPureErrors) {
-                                next(error.error);
-                                return;
-                            }
-
-                            next(error);
-                            return;
-                        }
-
-                        if (passPureErrors) {
-                            this.respond<Error | undefined>(error.error, res, error.code);
-                            return;
-                        }
-
-                        this.respond<IErrorType>(error, res, error.code);
+                    if (!this.proceedExecution<void>(error, invokeNextOnError, passPureErrors, res, next)) {
                         return;
                     }
                 }
 
+                // call next if control flow switch requires it
                 if (invokedNextAfterExecution) {
                     next();
                     return;
                 }
 
+                // no next needed to be called, respond with success and
+                // the result
                 this.respond<IResponse<ResultType>>(
-                    { code: 200, error: undefined, result, status: "Success" }, res, 200);
+                    { code: 200, result, status: "Success" }, res, 200);
 
             };
         }
 
+    /**
+     * invoke a awaitable function while catching all possible errors
+     * and replacing them with proper, sendable errors
+     * @param awaitable function that returns T when called with arguments
+     * @param context this context to invoke the awaitable function in
+     * @param args arguments to call the awaitable function with
+     */
     private async fetchPossibleErrors<T>(
         awaitable: (...args: any[]) => T | Promise<T>, context: any, ...args: any[]): Promise<T | IErrorType> {
             try { return await awaitable.apply(context, args); } catch (e) {
@@ -155,6 +160,42 @@ export abstract class ScaffoldedRequestHandler<RequestType extends Request, Sour
 
                 return { code: Errors.ServerError.code, status: Errors.ServerError.status, error: e } as IErrorType;
             }
+        }
+
+    /**
+     * detect errors in a result object and change behavior accordingly
+     * @param result object which can be the desired result or an error (either
+     * wrapped or completely pure)
+     * @param invokeNextOnError control-flow switch to invoke next if result is an error
+     * @param passPureErrors control-flow switch to not replace errors with sendable objects
+     * @param res expressjs response object
+     * @param next expressjs next function
+     */
+    private proceedExecution<T>(
+        result: T | IErrorType, invokeNextOnError: boolean,
+        passPureErrors: boolean, res: Response, next: NextFunction) {
+            if (isErrorType(result)) {
+
+                if (invokeNextOnError) {
+                    if (passPureErrors) {
+                        next(result.error);
+                        return false;
+                    }
+
+                    next(result);
+                    return false;
+                }
+
+                if (passPureErrors) {
+                    this.respond<{ error: Error | undefined }>({ error: result.error }, res, result.code);
+                    return false;
+                }
+
+                this.respond<IErrorType>(result, res, result.code);
+                return false;
+            }
+
+            return true;
         }
 
     private respond<ResponseType>(response: ResponseType, res: Response, status: number = 200) {
