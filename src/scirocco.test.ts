16dc93762533c4a3ec370e3ce5de7e972fdc24b8
jakoblorz <jakob@lorz.org>
2017-10-31 12:56:03 +0100

    added first test-case and mocha test scripts

--- src/scirocco.test.ts
+++ src/scirocco.test.ts
@@ -2,3 +2,15 @@ import * as assert from "assert";
 import * as mocha from "mocha";
 
 import * as sci from "./scirocco";
+
+describe("Errors", async () => {
+    describe("isErrorType()", async () => {
+        it("should detect Errors from Errors namespace as IErrorType", async () => {
+            assert.equal(sci.Errors.isErrorType(sci.Errors.ForbiddenError), true);
+            assert.equal(sci.Errors.isErrorType(sci.Errors.FormatError), true);
+            assert.equal(sci.Errors.isErrorType(sci.Errors.NotFoundError), true);
+            assert.equal(sci.Errors.isErrorType(sci.Errors.ServerError), true);
+            assert.equal(sci.Errors.isErrorType(sci.Errors.UnauthorizedError), true);
+        });
+    });
+});
